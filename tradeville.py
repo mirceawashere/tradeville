26ca3794ea8f2c7953ff3bb3b4a066f3950a3d03
mirceawashere <96563222+mirceawashere@users.noreply.github.com>
2025-01-23 21:10:24 +0200

    Update tradeville.py

--- tradeville.py
+++ tradeville.py
@@ -37,7 +37,7 @@ def get_market_value():
         total_value = quantity * market_price
 
         # Return JSON response
-        return jsonify({"total_value": total_value})
+        return total_value
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
